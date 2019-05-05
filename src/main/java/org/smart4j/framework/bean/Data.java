5c3169c29deb6366fb188f9dc85a7bb1b25a241f
dingming <13316098709@189.cn>
2019-05-05 22:37:30 +0800

    Initial commit

---
+++ src/main/java/org/smart4j/framework/bean/Data.java
@@ -0,0 +1,1 @@
+package org.smart4j.framework.bean;/** * @ClassName Data * @Description TODO * @Author mac * @Date 2019-05-04 07:18 PM * Version 1.0 **/public class Data {    private Object model;    public Data(Object model) {        this.model = model;    }    public Object getModel() {        return model;    }}
